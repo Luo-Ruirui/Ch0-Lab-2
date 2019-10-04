0217720304e6e04a2c8f8303a8f6afe56f6116be
108360207 <abl5968132@gmail.com>
2019-10-04 20:54:16 +0800

    Completed Lab1

--- source/Main.c
+++ source/Main.c
@@ -4,11 +4,6 @@
 int main(void)
 {
 	printf("Welcome to C!\n");
-<<<<<<< HEAD
-	printf("My student id is: 108360207\n");
-=======
-	printf("My name is Luo-Ruirui.\n");
->>>>>>> std_name
 	system("pause");
 	return 0;
 }
